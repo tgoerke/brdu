2d5a817fbc2319daaf6918c100e745a6a16f7c6d
Diego Jahn <diego.jahn@tu-dresden.de>
2019-07-16 16:39:20 +0200

    Fix 404 response when only upload/ is called.

--- brdu/fit/forms.py
+++ brdu/fit/forms.py
@@ -29,11 +29,11 @@ class UploadForm(forms.ModelForm):
         row = kwargs.pop('row') # get row parameter for form_action = reverse()
         super(UploadForm, self).__init__(*arg, **kwargs)
         
-        self.fields['file'].help_text = 'Upload your data in CSV format with <br /> column order as in the table on the left.'
-
         self.helper = FormHelper()
         self.helper.form_id = 'id-uploadForm'
         #self.helper.form_class = ''
         self.helper.form_method = 'post'
         self.helper.form_action = reverse('fit:upload', kwargs={'row': row}) # , css_class='btn btn-primary'
         self.helper.add_input(Submit('submit', 'Upload'))
+
+        self.fields['file'].help_text = 'Upload your data in CSV format with <br /> column order as in the table on the left.'
