2598360cd46d05449b51dd38bff6e6d9649ff746
Diego Jahn <diego.jahn@tu-dresden.de>
2019-07-22 16:39:12 +0200

    Add button icons.

--- brdu/fit/forms.py
+++ brdu/fit/forms.py
@@ -25,17 +25,6 @@ class InputForm(forms.ModelForm):
         self.error_class = MyErrorList # https://stackoverflow.com/questions/2125717/django-forms-error-class
 
         self.empty_permitted = True
-        
-        # Crispy Forms
-        self.helper = FormHelper()
-        self.helper.form_id = 'id-experimentalDataFormset'
-        #self.helper.form_class = ''
-        self.helper.form_method = 'post'
-        self.helper.form_action = reverse('fit:form', kwargs={'row': row})
-        self.helper.add_input(Submit('calc', 'Calculate', css_class='btn btn-primary'))
-        self.helper.add_input(Submit('clear', 'Clear all data'))
-        self.helper.add_input(Submit('add', 'Add 10 rows'))
-        self.helper.add_input(Submit('update', 'Clear empty lines'))
 
 class UploadForm(forms.ModelForm):
     class Meta:
@@ -50,14 +39,15 @@ class UploadForm(forms.ModelForm):
         #row = kwargs.pop('row') # get row parameter for form_action = reverse()
         super(UploadForm, self).__init__(*args, **kwargs)
         
+        """
         self.helper = FormHelper()
         
-        """self.helper.layout = Layout(
-            Fieldset('Or upload CSV file:',
-                Field('file',
-                ),
-            ),
-        )"""
+        # self.helper.layout = Layout(
+        #    Fieldset('Or upload CSV file:',
+        #        Field('file',
+        #        ),
+        #    ),
+        # )
 
         self.helper.form_id = 'id-uploadForm'
         #self.helper.form_class = ''
@@ -65,3 +55,4 @@ class UploadForm(forms.ModelForm):
         self.helper.form_action = reverse('fit:upload', kwargs={'row': row})
         self.helper.add_input(Submit('submit', 'Upload', css_class="btn-secondary"))
         self.fields['file'].help_text = 'Upload your data in CSV format with <br /> column order as in the table on the left.'
+        """
